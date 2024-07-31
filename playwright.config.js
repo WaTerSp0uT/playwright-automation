1e36893fee837c7b6a6f29be89cee6efba41ea3d
Yaroslav Kryvda <ericyarek@Yaroslavs-MacBook-Pro.local>
2024-07-30 21:43:51 -0400

    config set up

--- playwright.config.js
+++ playwright.config.js
@@ -35,7 +35,11 @@ module.exports = defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+         ...devices['Desktop Chrome'],
+        viewport: {width:1512, height:982 },
+        headless: false
+      },
     },
 /*
     {
